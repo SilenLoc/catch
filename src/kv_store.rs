386d201b2d71b79ee4185b0e084607e40a41bf43
Boris <bschwarz@emotions.ch>
2026-02-11 20:57:09 +0100

    Add DELETE endpoint

--- src/kv_store.rs
+++ src/kv_store.rs
@@ -1,6 +1,8 @@
 use std::{collections::HashMap, sync::Mutex};
 
-use actix_web::{HttpRequest, HttpResponse, Responder, get, http::header::HeaderValue, post, web};
+use actix_web::{
+    HttpRequest, HttpResponse, Responder, delete, get, http::header::HeaderValue, post, web,
+};
 
 pub type KeyValueStore = Mutex<HashMap<String, HashMap<String, String>>>;
 
@@ -64,4 +66,28 @@ pub async fn set_kv(
     inner.insert(key.clone(), value);
 
     HttpResponse::Ok().body("Key set")
+}
+
+#[delete("/kv/{key}")]
+pub async fn delete_kv(
+    req: HttpRequest,
+    key: web::Path<String>,
+    store: web::Data<KeyValueStore>,
+) -> impl Responder {
+    let static_def = &HeaderValue::from_str("default").unwrap();
+    let context = req
+        .headers()
+        .get("X-Context")
+        .unwrap_or(static_def)
+        .to_str()
+        .unwrap();
+
+    let mut store = store.lock().unwrap();
+
+    let Some(inner) = store.get_mut(context) else {
+        return HttpResponse::NoContent();
+    };
+
+    inner.remove(&key.to_string());
+    HttpResponse::NoContent()
 }
