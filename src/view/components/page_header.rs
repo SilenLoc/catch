30012558db707c76ac7de8c6b9d5497cf54354ac
Silen Locatelli <silen.locatelli@gmx.ch>
2026-02-26 21:59:53 +0100

    fix: titles

--- src/view/components/page_header.rs
+++ src/view/components/page_header.rs
@@ -4,7 +4,7 @@ use maud::html;
 pub fn render(title: &str, subtitle: &str) -> maud::Markup {
     html! {
         header class="mb4 pb3 bb b--black-40 flex items-center justify-between" {
-            h2 class="f3 f2-ns fw6 ma0 white" { (title) }
+            h4 class="f3 f3-ns fw4 ma0 white" { (title) }
             span class="f6 fw4 white" { (subtitle) }
         }
     }
